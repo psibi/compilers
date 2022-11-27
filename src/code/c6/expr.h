b2be2a71b534fd275fca7b083663003eea21f306
Sibi Prabakaran <sibi@psibi.in>
2022-11-27 15:00:39 +0530

    Original code from c5

---
+++ src/code/c6/expr.h
@@ -0,0 +1,14 @@
+typedef enum {
+  EXPR_ADD,
+  EXPR_SUBTRACT,
+  EXPR_DIVIDE,
+  EXPR_MULTIPLY,
+  EXPR_VALUE
+} expr_t;
+
+struct expr {
+  expr_t kind;
+  struct expr *left;
+  struct expr *right;
+  int value;
+};
